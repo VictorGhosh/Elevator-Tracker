bc7e150c5ace9b2dbfefa7c2baa0e581cb0c8b22
Victor <18vg5@queensu.ca>
2023-06-12 14:00:10 -0400

    Accelerometer working

--- src/elevator/elevator.ino
+++ src/elevator/elevator.ino
@@ -6,6 +6,7 @@
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BMP3XX.h"
+#include "SparkFun_BMA400_Arduino_Library.h"
 #include <RH_RF95.h>
 
 #define CS 9
@@ -13,7 +14,10 @@
 #define MOSI 4 // SDA: Override due to radio problem
 
 Adafruit_BMP3XX bmp;
+BMA400 bma;
 RH_RF95 rf95(12, 6);
+
+uint8_t i2cAddress = BMA400_I2C_ADDRESS_DEFAULT; // 0x14
 
 bool debug = false;
 int readings = 8;
@@ -27,13 +31,25 @@ void setup() {
   
   SerialUSB.println("> Elevator: Begin setup...");
 
+  Wire.begin();
+
   if (!bmp.begin_SPI(CS, SCK, MISO, MOSI)) {
     SerialUSB.println("> Elevator: BMP setup failed!");
+    return;
   }
 
   if (rf95.init() == false){
     SerialUSB.println("> Elevator: Radio setup failed!");
+    return;
   }
+
+  if (bma.beginI2C(i2cAddress) != BMA400_OK) {
+    SerialUSB.println("> Elevator: Accelerometer setup failed!");
+    return;
+  }
+
+  // This is a real thing but I dont know how to make it work.
+  // bma.setAutoLowPower(bma400_auto_lp_conf *config)
 
   bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
   bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
@@ -57,45 +73,39 @@ void setup() {
 }
 
 void loop() {
-  if (!bmp.performReading()) {
-    SerialUSB.println("> Elevator: BMP reading failed!");
-  }
-
   float pres = getPressure(readings);
-  float temp = bmp.readTemperature();
+  float acc = getAcceleration();
 
   uint8_t buf[sizeof(float) * 2];
-  // uint8_t buf[sizeof(float)];
   ::memcpy(buf, &pres, sizeof(float));
-  ::memcpy(buf + sizeof(float), &temp, sizeof(float));
+  ::memcpy(buf + sizeof(float), &acc, sizeof(float));
 
   if (debug) {
     SerialUSB.print("> Elevator: transmitting (");
     SerialUSB.print(pres);
     SerialUSB.print("f, ");
-    SerialUSB.print(temp);
+    SerialUSB.print(acc);
     SerialUSB.println("f)");
-    // digitalWrite(PIN_LED_TXL, LOW); // LED is backwards from factory
   }
-  digitalWrite(PIN_LED_TXL, LOW); // LED is backwards from factory
-
 
   rf95.send(buf, sizeof(buf));
   rf95.waitPacketSent();
 
-  // if (debug) { digitalWrite(PIN_LED_TXL, HIGH); }
-  digitalWrite(PIN_LED_TXL, HIGH);
-
-  delay(100);
+  delay(500);
 }
 
 float getPressure(int r) {
   float sum = 0;
   for (int i = 0; i < r; i++) {
     if (!bmp.performReading()) {
-      SerialUSB.println("> BMP reading failed!");
+      SerialUSB.println("> Elevator: BMP reading failed!");
     }
     sum += bmp.readPressure();
   }
   return sum / r;
+}
+
+float getAcceleration() {
+  bma.getSensorData();
+  return sqrt(sq(bma.data.accelX) + sq(bma.data.accelY) + sq(bma.data.accelZ));
 }
