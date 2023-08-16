143892d02e1d3f796d20b83964f06082475c93aa
Victor <18vg5@queensu.ca>
2023-08-15 23:00:30 -0400

    New post project data

--- data/Final/graph.sh
+++ data/Final/graph.sh
@@ -22,6 +22,26 @@ set arrow from 1,95 rto graph 1,0 ls 1 nohead # 7
 
 plot 'June19-Numbered.txt' using 4:($2-$1) with line axis x1y1 notitle
 
+
+# STAIR/FOOT DATA 2
+set output 'footDat2.png'
+set title 'Data on Foot Post Project' offset 0,-1
+set xlabel 'Reading Number'
+set ylabel 'Base Pressure - Lobby Pressure (hPa)'
+set xtics nomirror
+set ytics nomirror
+
+set arrow from 1,-145 rto graph 1,0 ls 1 nohead # 1
+set arrow from 1,-102 rto graph 1,0 ls 1 nohead # 2
+set arrow from 1,-66 rto graph 1,0 ls 1 nohead # 3
+set arrow from 1,-24 rto graph 1,0 ls 1 nohead # 4
+set arrow from 1,13 rto graph 1,0 ls 1 nohead # 5
+set arrow from 1,53 rto graph 1,0 ls 1 nohead # 6
+set arrow from 1,95 rto graph 1,0 ls 1 nohead # 7
+
+plot 'Aug15.txt' using 8:($2-$3) with line axis x1y1 notitle
+
+
 # ELEVATOR DATA
 set terminal pngcairo size 2048,820 font 'Helvetica,22'
 set output 'elevatorDat.png'
